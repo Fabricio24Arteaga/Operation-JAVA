35d3cb0d0706b72c9676f739f4a94d9effd5d96c
Fabricio24Arteaga <fabricio.artev@gmail.com>
2025-04-02 18:12:52 -0500

    Update Ejemplo01.java

--- src/Sesion01/Ejemplo01.java
+++ src/Sesion01/Ejemplo01.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class Ejemplo01 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        float n1,n2,sum,res,mul,div;
+        float n1,n2,n3,sum,res,mul,div;
         
         System.out.print("Digite un numeros: ");
         n1 = entrada.nextFloat();
         System.out.print("Digite un numeros: ");
         n2 = entrada.nextFloat();
+        System.out.print("Digite un numeros: ");
+        n3 = entrada.nextFloat();
         
-        sum=n1+n2;
-        res=n1-n2;
-        mul=n1*n2;
-        div=n1/n2;
+        sum=n1+n2+n3;
+        res=n1-n2-n3;
+        mul=n1*n2*n3;
+        div=n1/n2/n3;
         
         System.out.println("la suma es: "+sum);
         System.out.println("la resta es: "+res);
